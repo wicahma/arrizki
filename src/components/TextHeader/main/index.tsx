919e9e763f3a4efdeffc94951670b9060013d9b0
wicahma <teguh.dc.kusuma@gmail.com>
2023-06-19 09:46:28 +0700

    update: some random stuff

--- src/components/TextHeader/main/index.tsx
+++ src/components/TextHeader/main/index.tsx
@@ -9,7 +9,7 @@ interface TextHeaderProps {
 const index = (props: TextHeaderProps) => {
   const { title, subtitle, className } = props;
   return (
-    <div className={`text-center mb-7 ${className}`}>
+    <div className={`text-center mb-7 px-5 ${className}`}>
       <h1 className="text-4xl font-bold font-serif">{title}</h1>
       <p className="text-xl font-normal">{subtitle}</p>
     </div>
